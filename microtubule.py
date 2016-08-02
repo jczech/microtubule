086dea2fe790ea1faf78ee94ead44900aba1f81b
Jacob Czech <czech.jacob@gmail.com>
2016-08-02 15:01:19 -0400

    Add multiple segments of microtubule.
    I'm just adding Anirban's changes.

--- microtubule.py
+++ microtubule.py
@@ -4,8 +4,7 @@ from mathutils import Vector
 from bpy_extras.object_utils import AddObjectHelper, object_data_add
 
 Rm=0.5
-Lm=50
-x0=-5.0
+
 
 D=pi*Rm/sqrt(40)
 dth=2*pi/13
@@ -17,9 +16,9 @@ mat_alpha.diffuse_color=(0.9, 0.0, 0.0)
 mat_beta = bpy.data.materials.new('beta_tubulin')
 mat_beta.diffuse_color=(0.0, 0.0, 0.9)
 
-def makeMicrotubuleStrand(zBottom=0.0,nBeads=26,isAlphaAtStart=True, axis='z'):
+def makeMicrotubuleStrand(start=Vector((0,0,0)),nBeads=26,isAlphaAtStart=True, axis='z', name="Microtubule"):
     th=0
-    zc=zBottom
+    zc=0
     snippetLength=0
     isAlpha=isAlphaAtStart
     alpha_verts = []
@@ -35,7 +34,8 @@ def makeMicrotubuleStrand(zBottom=0.0,nBeads=26,isAlphaAtStart=True, axis='z'):
            pv=Vector((yc, zc, xc))
         else:
            pv=Vector((xc, yc, zc))
-
+        pv=pv+start
+        
         if(not isAlpha):
             alpha_verts.append(pv)
         else:
@@ -51,79 +51,101 @@ def makeMicrotubuleStrand(zBottom=0.0,nBeads=26,isAlphaAtStart=True, axis='z'):
 
     alpha_edges = []
     alpha_faces = []
+    alpha_mesh = bpy.data.meshes.new(name=name+"_AlphaVerts")
+    alpha_mesh.from_pydata(alpha_verts, alpha_edges, alpha_faces)
+    alpha_obj = object_data_add(bpy.context, alpha_mesh)
+    
     beta_edges = []
     beta_faces = []
-
-    alpha_mesh = bpy.data.meshes.new(name="AlphaVerts")
-    alpha_mesh.name = "AlphaVerts"
-    alpha_mesh.from_pydata(alpha_verts, alpha_edges, alpha_faces)
-    
-    beta_mesh = bpy.data.meshes.new(name="BetaVerts")
-    beta_mesh.name = "BetaVerts"
+    beta_mesh = bpy.data.meshes.new(name=name+"_BetaVerts")
     beta_mesh.from_pydata(beta_verts, beta_edges, beta_faces)
-
-    alpha_obj = object_data_add(bpy.context, alpha_mesh)
     beta_obj = object_data_add(bpy.context, beta_mesh)
 
-def makeMicrotubule(zBottom,length,axis):
+def makeMicrotubule(startAt, length, axis='z', name="Microtubule"):
     
-    bpy.context.scene.cursor_location = (0, 0, 0)
-    
+    start=Vector(startAt)
     nBeads=round(length/dz)
-    makeMicrotubuleStrand(zBottom=zBottom,nBeads=nBeads,isAlphaAtStart=True, axis=axis)
-    makeMicrotubuleStrand(zBottom=zBottom+D,nBeads=nBeads,isAlphaAtStart=False, axis=axis)
-    makeMicrotubuleStrand(zBottom=zBottom+2*D,nBeads=nBeads,isAlphaAtStart=True, axis=axis)
+
+    if axis=='x':
+       shift=Vector((D,0,0))
+       s1u1c= (start[0], start[1]+Rm, start[2])
+       s2u1c= (start[0]+D, start[1]+Rm, start[2])
+    elif axis=='y':
+       shift=Vector((0,D,0))
+       s1u1c= (start[0], start[1], start[2]+Rm)
+       s2u1c= (start[0], start[1]+D, start[2]+Rm)
+    else:
+       shift=Vector((0,0,D))
+       s1u1c= (start[0]+Rm, start[1], start[2])
+       s2u1c= (start[0]+Rm, start[1]+Rm, start[2]+D)
+
+    bpy.context.scene.cursor_location = (0,0,0)
+    makeMicrotubuleStrand(start=start,nBeads=nBeads,isAlphaAtStart=True, axis=axis,name=name+"_Strand1")
+    makeMicrotubuleStrand(start=start+shift,nBeads=nBeads,isAlphaAtStart=False, axis=axis,name=name+"_Strand2")
+    makeMicrotubuleStrand(start=start+2*shift,nBeads=nBeads,isAlphaAtStart=True, axis=axis,name=name+"_Strand3")
 
     objects = bpy.data.objects
     bpy.ops.object.select_all(action='DESELECT')
-    objects["AlphaVerts"].select = True
-    objects["AlphaVerts.001"].select = True
-    objects["AlphaVerts.002"].select = True
-    bpy.context.scene.objects.active = objects["AlphaVerts"]
+    objects[name+"_Strand1"+"_AlphaVerts"].select = True
+    objects[name+"_Strand2"+"_AlphaVerts"].select = True
+    objects[name+"_Strand3"+"_AlphaVerts"].select = True
+    bpy.context.scene.objects.active = objects[name+"_Strand1"+"_AlphaVerts"]
     bpy.ops.object.join()
+    objav=objects[name+"_Strand1"+"_AlphaVerts"]
+    objav.name=name+"_AlphaVerts"
     
     bpy.ops.object.select_all(action='DESELECT')
-    objects["BetaVerts"].select = True
-    objects["BetaVerts.001"].select = True
-    objects["BetaVerts.002"].select = True
-    bpy.context.scene.objects.active = objects["BetaVerts"]
+    objects[name+"_Strand1"+"_BetaVerts"].select = True
+    objects[name+"_Strand2"+"_BetaVerts"].select = True
+    objects[name+"_Strand3"+"_BetaVerts"].select = True
+    bpy.context.scene.objects.active = objects[name+"_Strand1"+"_BetaVerts"]
     bpy.ops.object.join()
+    objbv=objects[name+"_Strand1"+"_BetaVerts"]
+    objbv.name=name+"_BetaVerts"
     
     bpy.ops.object.select_all(action='DESELECT')
-    objects["AlphaVerts"].select = True
-    bpy.context.scene.cursor_location = (zBottom+D, Rm, 0)
+    objav.select = True
+    bpy.context.scene.cursor_location = s1u1c
     bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
     
     bpy.ops.object.select_all(action='DESELECT')
-    objects["BetaVerts"].select = True
-    bpy.context.scene.cursor_location = (zBottom, Rm, 0)
+    objbv.select = True
+    bpy.context.scene.cursor_location = s2u1c
     bpy.ops.object.origin_set(type='ORIGIN_CURSOR')
-  
-    bpy.ops.mesh.primitive_ico_sphere_add(subdivisions=3, size=D/2, enter_editmode=True, location=(zBottom+D, Rm, 0))
+    
+    bpy.ops.mesh.primitive_ico_sphere_add(subdivisions=3, size=D/2, enter_editmode=True, location=s1u1c)
     bpy.ops.mesh.faces_shade_smooth()
     bpy.ops.object.editmode_toggle()
     bpy.context.object.data.materials.append(mat_alpha)
-    bpy.data.objects["Icosphere"].name = "AlphaUnit"
+    objau=bpy.data.objects["Icosphere"]
+    objau.name = name+"_AlphaUnit"
     
-    bpy.ops.mesh.primitive_ico_sphere_add(subdivisions=3, size=D/2, enter_editmode=True, location=(zBottom, Rm, 0))
+    bpy.ops.mesh.primitive_ico_sphere_add(subdivisions=3, size=D/2, enter_editmode=True, location=s2u1c)
     bpy.ops.mesh.faces_shade_smooth()
     bpy.ops.object.editmode_toggle()
     bpy.context.object.data.materials.append(mat_beta)
-    bpy.data.objects["Icosphere"].name = "BetaUnit"
+    objbu=bpy.data.objects["Icosphere"]
+    objbu.name = name+"_BetaUnit"
     
     bpy.ops.object.select_all(action='DESELECT')    
-    bpy.context.scene.objects.active = objects["AlphaVerts"]
-    objects["AlphaUnit"].select = True
-    objects["AlphaVerts"].select = True
+    bpy.context.scene.objects.active = objav
+    objau.select = True
+    objav.select = True
     bpy.ops.object.parent_set(type='VERTEX')
-    objects["AlphaVerts"].dupli_type = 'VERTS'
+    objav.dupli_type = 'VERTS'
 
     bpy.ops.object.select_all(action='DESELECT')    
-    bpy.context.scene.objects.active = objects["BetaVerts"]
-    objects["BetaUnit"].select = True
-    objects["BetaVerts"].select = True
+    bpy.context.scene.objects.active = objbv
+    objbu.select = True
+    objbv.select = True
     bpy.ops.object.parent_set(type='VERTEX')
-    objects["BetaVerts"].dupli_type = 'VERTS'
+    objbv.dupli_type = 'VERTS'
+    
 
+Lm=15
+r0=(-8 ,-3 ,0)
+makeMicrotubule(r0,Lm,'x', name="MT1")
 
-makeMicrotubule(x0,Lm,'x')
+Lm=10
+r0=(-7 ,2, 0)
+makeMicrotubule(r0,Lm,'x', name="MT2")
